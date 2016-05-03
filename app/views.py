237b133c7810125ab67ab9f561f1e4ca6e142630
Kathure <lizkathure@gmail.com>
2016-05-03 17:34:21 +0300

    control staements in templates

--- app/views.py
+++ app/views.py
@@ -1,6 +1,26 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Kathure'}  # fake user
+    posts = [#fake array of posts
+      {
+            "author":{"nickname":"Johnny"},
+            "body":"Beautiful day in Portland !"
+        },
+        {
+            "author":{"nickname":"Susan"},
+            "body": "The Avengers movie was super cool!"
+        },
+        {
+            "author":{"nickname":"Liz"},
+            "body":"Captain America Civil War premieres on Friday"
+        }
+
+    ]
+    return render_template('index.html',
+                          user=user,
+                          posts = posts
+                          )
