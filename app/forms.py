0c43bf35be3f5b1f40bd09bcfa12e9231362eb51
Kathure <lizkathure@gmail.com>
2016-05-04 11:47:48 +0300

    forms.py

---
+++ app/forms.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+from flask.ext.wtf import Form
+from wtforms import StringField, BooleanField
+from wtforms.validators import DataRequired
+
+class LoginForm(Form):
+    openid = StringField('openid', validators=[DataRequired()])
+    reme
