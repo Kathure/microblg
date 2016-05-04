1a975ca29bcf606c6f07910197766b761ece81ec
Kathure <lizkathure@gmail.com>
2016-05-04 11:59:25 +0300

    commented on imported classes

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 
-from flask.ext.wtf import Form
-from wtforms import StringField, BooleanField
-from wtforms.validators import DataRequired
+from flask.ext.wtf import Form #imports Form class
+from wtforms import StringField, BooleanField #imports field classes we need
+from wtforms.validators import DataRequired #imports a validator
 
 class LoginForm(Form):
     openid = StringField('openid', validators=[DataRequired()])
-    reme
+    remember_me = BooleanField('remember_me', default=False)
