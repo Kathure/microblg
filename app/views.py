d0f3d54e577cc0aea11357e410f6d4186a0b22fb
Kathure <lizkathure@gmail.com>
2016-05-09 13:07:13 +0300

    validation error handling

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -33,9 +34,19 @@ def about():
 
 
     return render_template('about.html',
-
                           )
 @app.route('/')
 @app.route('/gallery')
 def gallery():
     return render_template('gallery.html',)
+
+@app.route('/login', methods = ['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s" remember_me=%s' %
+            (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html',
+                          title = 'Sign In',
+                          form=form)
